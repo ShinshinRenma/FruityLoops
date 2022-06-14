e698e534073047dbc1a0be742f7165794a26789b
ShinshinRenma <ryan.andrew.layman@gmail.com>
2022-06-14 12:51:49 -1000

    Initial commit

---
+++ src/main/java/com/codingdojo/fruityloops/FruityloopsApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.fruityloops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityloopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityloopsApplication.class, args);
+	}
+}
